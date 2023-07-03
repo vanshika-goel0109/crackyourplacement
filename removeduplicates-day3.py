286416705f094b16591ec743b93eb7b585d4d95c
Vanshika Goel <65799147+vanshika-goel0109@users.noreply.github.com>
2023-07-04 01:10:49 +0530

    Create removeduplicates-day3.py

---
+++ removeduplicates-day3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i=0
+        j=0
+        while(j<len(nums)):
+            if(nums[i]==nums[j]):
+                j+=1
+            else:
+                i+=1
+                nums[i]=nums[j]
+               
+        return i+1
